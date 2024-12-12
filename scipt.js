e5069f954b52929f740df5ba9e86fcc97335f652
Gh0st6764 <dpratt28@students.stab.org>
2024-12-12 11:44:23 -0500

    script.js

--- scipt.js
+++ scipt.js
@@ -1,1 +1,11 @@
-var url = "hhttps://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Culture%20%26%20Entertainment/Best%20Selling%20Video%20Games.csvttps://raw.githubusercontent.com/b-mcavoy/datasets/main/Animals/Dogs.csv";
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Culture%20%26%20Entertainment/Best%20Selling%20Video%20Games.csv";
+
+var platforms = getColumn(url, 3);
+var developer = getColumn(url, 5);
+var releaseDate = getColumn(url, 4);
+
+function createList(platform, recent, developer){
+    document.getElementById('output').innerHTMl = "";
+    var matchingGames
+    var matchingImages 
+}
