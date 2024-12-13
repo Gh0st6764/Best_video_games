e782a913fd378866c47f17b360e1c877bdf81c49
Gh0st6764 <dpratt28@students.stab.org>
2024-12-13 13:05:29 -0500

    script.js

--- scipt.js
+++ scipt.js
@@ -1,11 +1,52 @@
 var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Culture%20%26%20Entertainment/Best%20Selling%20Video%20Games.csv";
 
+//this links all the columns and lists to variables 
+var videoGames = getColumn(url, 1);
 var platforms = getColumn(url, 3);
-var developer = getColumn(url, 5);
 var releaseDate = getColumn(url, 4);
+var developer = getColumn(url, 5);
 
-function createList(platform, recent, developer){
+var displayGames = [];
+
+//this is the most important variable, this is the final product. 
+//after every input is completed it is added up into matching games
+//to be displayed
+var matchingGames = [];
+
+/*function createList(platform, recent, developer){
     document.getElementById('output').innerHTMl = "";
-    var matchingGames
-    var matchingImages 
+} */
+
+var uniquePlatforms
+ for (var i = 0; i < platforms.length; i++){
+ if(!uniquePlatforms.includes(platforms[i])){
+    uniquePlatforms.push(platforms[i])
+ }
 }
+var uniquePlatformsCounts = [];
+for(var i = 0; i < uniquePlatforms.length; i++){
+    var count = 0;
+for(var j = 0; j < platforms.length; j++){
+    if(uniquePlatforms[i] == parties[j]){
+        count++
+        }
+    }
+    uniquePlatformsCounts.push(count);
+}
+
+var max = 0; 
+var platformWithMost = "";
+for(var i = 0; i < uniquePlatformsCounts.length; i++){
+    if(uniquePlatformsCounts[i] > max);
+    platformWithMost = uniquePlatforms[i]
+}
+console.log(uniquePlatforms)
+   
+    console.log(platforms);
+    console.log(uniquePlatforms);
+
+    // var platformSelect = document.getElementById("platforms")
+    //     for (var i = 0; i < uniquePlatforms.length; i++)
+    //         var el = document.createElement("option")
+
+    //         el.innerHTML =
